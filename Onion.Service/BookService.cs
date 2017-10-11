83de375f7e720e9c9ffd1dedb78b2970717552ad
Jamie Taylor <seppydude@gmail.com>
2017-10-11 22:13:40 +0100

    Added Services layer

---
+++ Onion.Service/BookService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Onion.Data.Entities;
+using Onion.Repo.Interfaces;
+using Onion.Service.Interfaces;
+
+namespace Onion.Service
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> _bookRepository;
+        private readonly IRepository<BookSeries> _bookSeriesRepository;
+
+        public BookService(IRepository<Book> bookRepository,
+            IRepository<BookSeries> bookSeriesRepository)
+        {
+            _bookRepository = bookRepository;
+            _bookSeriesRepository = bookSeriesRepository;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookRepository.GetAll();
+        }
+
+        public Book GetBook(int id)
+        {
+            return _bookRepository.Get(id);
+        }
+
+        public void InsertBook(Book book)
+        {
+            _bookRepository.Insert(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _bookRepository.Update(book);
+        }
+
+        public void DeleteBook(int id)
+        {
+            var bookSeries = _bookSeriesRepository.Get(id);
+            _bookSeriesRepository.Delete(bookSeries);
+
+            var user = _bookRepository.Get(id);
+            _bookRepository.Delete(user);
+            _bookRepository.SaveChanges();
+        }
+    }
+}
---
+++ Onion.Service/Interfaces/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Onion.Service.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
